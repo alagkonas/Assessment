a5a1c9344698f18e76634e9ebb3b4d857fd780ef
Alexandros Lagkonas <lagkonas26@gmail.com>
2022-06-16 19:26:09 +0300

    Private routing added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,7 +14,9 @@ function App() {
         <div className='App'>
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<PrivateRoute />}>
+              <Route path='/' element={<Home />} />
+            </Route>
             <Route path='/sign_in' element={<SignIn />} />
           </Routes>
         </div>
